5555a2fa4870bf4695e73ca7d5854ad94d40fbd7
akshay <aceakshay@gmail.com>
2025-01-11 20:10:18 +0100

    feat : code that simply records and repeats

---
+++ play.py
@@ -0,0 +1,101 @@
+import time
+import pyautogui
+
+time.sleep(1)
+
+pyautogui.moveTo(0, 1068)
+pyautogui.mouseDown()
+time.sleep(1)
+
+pyautogui.moveTo(0, 1068)
+pyautogui.mouseUp()
+time.sleep(2.379816508293152)
+
+pyautogui.moveTo(153, 1069)
+pyautogui.mouseDown()
+time.sleep(0.25224077701568604)
+
+pyautogui.moveTo(153, 1069)
+pyautogui.mouseUp()
+time.sleep(4.205602598190308)
+
+pyautogui.moveTo(249, 588)
+pyautogui.mouseDown()
+time.sleep(0.2381469011306763)
+
+pyautogui.moveTo(249, 588)
+pyautogui.mouseUp()
+time.sleep(3.150814723968506)
+
+pyautogui.press('w')
+time.sleep(0.16799747943878174)
+
+pyautogui.press('w')
+time.sleep(0.5913222789764405)
+
+pyautogui.press('w')
+time.sleep(1.5506347656250001)
+
+pyautogui.press('.')
+time.sleep(0.2844993352890015)
+
+pyautogui.press('c')
+time.sleep(0.20877006053924563)
+
+pyautogui.press('l')
+time.sleep(0.16484143733978274)
+
+pyautogui.press('e')
+time.sleep(0.2385972023010254)
+
+pyautogui.press('r')
+time.sleep(0.16483566761016846)
+
+pyautogui.press('o')
+time.sleep(0.18731060028076174)
+
+pyautogui.press('n')
+time.sleep(1.077683997154236)
+
+pyautogui.press('.')
+time.sleep(0.33894984722137456)
+
+pyautogui.press('i')
+time.sleep(0.13445646762847902)
+
+pyautogui.press('n')
+time.sleep(1.1877413272857666)
+
+pyautogui.press('enter')
+time.sleep(4.6827455997467045)
+
+pyautogui.moveTo(1145, 1061)
+pyautogui.mouseDown()
+time.sleep(0.24866039752960206)
+
+pyautogui.moveTo(1145, 1061)
+pyautogui.mouseUp()
+time.sleep(4.187759184837342)
+
+pyautogui.moveTo(864, 993)
+pyautogui.mouseDown(button='right')
+time.sleep(1.9801774978637696)
+
+pyautogui.moveTo(864, 993)
+pyautogui.mouseUp(button='right')
+time.sleep(2.0129128456115724)
+
+pyautogui.moveTo(1228, 338)
+pyautogui.mouseDown()
+time.sleep(0.24762656688690188)
+
+pyautogui.moveTo(1228, 338)
+pyautogui.mouseUp()
+time.sleep(3.524811744689942)
+
+pyautogui.moveTo(800, 963)
+pyautogui.mouseDown(button='right')
+time.sleep(4.9956889152526855)
+
+pyautogui.moveTo(800, 963)
+pyautogui.mouseUp(button='right')
